068172cddaf3cfae742084aac141ceb50e01bf03
Saad Ata <s.ata@fetchrewards.com>
2023-10-26 13:29:25 -0500

    seperated code into different classes

--- src/main/java/edu/uab/groupassignment/DashboardSingleton.java
+++ src/main/java/edu/uab/groupassignment/DashboardSingleton.java
@@ -15,135 +15,29 @@ public class DashboardSingleton {
 
     // Singleton Declaration
     private static final DashboardSingleton instance = new DashboardSingleton();
+    public ItemController itemController;
+    public ControlPanelView panelView;
 
     // Private Constructor
     private DashboardSingleton() {
-        currentSelectedItem = itemsRoot;
+        itemController = new ItemController();
+        panelView = new ControlPanelView(itemController);
     }
 
-    // Main components
-    public HBox mainHBox;
-    private VBox leftComponents;
-
-    // Hierarchy components
-    private TreeView<FarmItem> itemsTree = new TreeView<>();
-    private FarmItem itemsRoot = new FarmItem(
-            true,
-            0,
-            0,
-            1000,
-            600,
-            50,
-            "Root",
-            null
-    );
-    private TreeItem<FarmItem> treeRoot = new TreeItem(itemsRoot);
-    private FarmItem currentSelectedItem;
-
-    // Config panel components
-    private GridPane configPane = new GridPane();
-    private Label warningLabel = new Label();
-    private TextField nameTextField;
-    private TextField priceTextField;
-    private TextField locationXTextField;
-    private TextField locationYTextField;
-    private TextField widthTextField;
-    private TextField heightTextField;
-    private final Button saveConfigBtn = new Button("Save");
-    private final Button deleteConfigBtn = new Button("Delete");
-    private final CheckBox addAsChildCheckBox = new CheckBox("Save Component as New Child");
-    private final Button visitSelectedWithDroneBtn = new Button("Visit Selected with Drone");
-    private final CheckBox isContainer = new CheckBox("Is Container");
-
-    // Visualizer components
-    private Group visGroup;
+    // Main component
+    private HBox mainHBox;
 
     public void init() {
-        testAdds();
-        updateItems();
-        expandTreeView(treeRoot);
-
-        isContainer.setDisable(!addAsChildCheckBox.isSelected());
-        isContainer.setAccessibleText("Must be new item");
-
-        // Set selected item by clicking in TreeView
-        itemsTree.setOnMouseClicked(event -> {
-            TreeItem<FarmItem> selTreeItem = itemsTree.getSelectionModel().getSelectedItem();
-            if (selTreeItem != null) {
-                currentSelectedItem = selTreeItem.getValue();
-                addAsChildCheckBox.setDisable(!currentSelectedItem.isContainer);
-                nameTextField.setText(currentSelectedItem.getName());
-                priceTextField.setText(String.valueOf(currentSelectedItem.price));
-                locationXTextField.setText(String.valueOf(currentSelectedItem.getX()));
-                locationYTextField.setText(String.valueOf(currentSelectedItem.getY()));
-                widthTextField.setText(String.valueOf(currentSelectedItem.getWidth()));
-                heightTextField.setText(String.valueOf(currentSelectedItem.getHeight()));
-                isContainer.setSelected(currentSelectedItem.isContainer);
-            }
-        });
-        // Custom cell factory to display only the name in the TreeView
-        itemsTree.setCellFactory(new Callback<>() {
-            @Override
-            public TreeCell<FarmItem> call(TreeView<FarmItem> param) {
-                return new TreeCell<>() {
-                    @Override
-                    protected void updateItem(FarmItem item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (item == null || empty) {
-                            setText("");
-                        } else {
-                            setText(item.getName());
-                        }
-                    }
-                };
-            }
-        });
-
         // Add the main section dedicated to visualization
-        visGroup = new Group(itemsRoot);
+        Group visGroup = new Group(itemController.itemsRoot);
+        VBox leftComponents = new VBox(
+                20,
+                itemController.itemsTree,
+                panelView.warningLabel,
+                panelView.configPane
+        );
         visGroup.minHeight(700);
         visGroup.minWidth(700);
-
-        addAsChildCheckBox.setOnMouseClicked(event -> {
-            isContainer.setDisable(!addAsChildCheckBox.isSelected());
-        });
-
-        // Add the section dedicated to controls
-        updateConfigPanel();
-        // Save functionality
-        saveConfigBtn.setOnMouseClicked(event -> {
-            if (addAsChildCheckBox.isSelected() && currentSelectedItem.isContainer) {
-                saveAsNewItem();
-                addAsChildCheckBox.setSelected(false);
-            } else {
-                currentSelectedItem.setName(nameTextField.getText());
-                currentSelectedItem.price = Double.valueOf(priceTextField.getText());
-                currentSelectedItem.setNewCoordinates(
-                        abs(Double.valueOf(locationXTextField.getText())),
-                        abs(Double.valueOf(locationYTextField.getText()))
-                );
-                currentSelectedItem.setNewDimentions(
-                        abs(Double.valueOf(widthTextField.getText())),
-                        abs(Double.valueOf(heightTextField.getText()))
-                );
-            }
-            updateItems();
-            expandTreeView(treeRoot);
-        });
-
-        // Delete functionality
-        deleteConfigBtn.setOnMouseClicked(event -> {
-
-                FarmItem toDelete = currentSelectedItem;
-                FarmItem parentItem = toDelete.getItemParent();
-                parentItem.removeChildItem(toDelete);
-                updateItems();
-                expandTreeView(treeRoot);
-                warningLabel.setText("Deleted");
-
-        });
-
-        leftComponents = new VBox(20, itemsTree, warningLabel, configPane);
         leftComponents.setSpacing(20);
         leftComponents.setPadding(new Insets(20, 20, 20, 20));
         leftComponents.setAlignment(Pos.TOP_CENTER);
@@ -152,115 +46,12 @@ public class DashboardSingleton {
         mainHBox = new HBox(20, leftComponents, visGroup);
     }
 
-    // Initializes the Items tree, along with adding some test items/containers
-    private void updateItems() {
-        treeRoot = new TreeItem(itemsRoot);
-        for (FarmItem item : itemsRoot.getContainedItems()) {
-            populateTree(treeRoot, item);
-        }
-        itemsTree.setRoot(treeRoot);
-    }
-
-    // Adds the item with the specifications as a child of the currentSelectedItem, draws it on screen.
-    private void saveAsNewItem() {
-        FarmItem newItem = new FarmItem(
-                isContainer.isSelected(),
-                Double.valueOf(locationXTextField.getText()),
-                Double.valueOf(locationYTextField.getText()),
-                Double.valueOf(widthTextField.getText()),
-                Double.valueOf(heightTextField.getText()),
-                Double.valueOf(priceTextField.getText()),
-                nameTextField.getText(),
-                currentSelectedItem
-        );
-
-        newItem.setParent(currentSelectedItem);
-        currentSelectedItem.addChildItem(newItem);
-        System.out.println(currentSelectedItem.getContainedItems());
-    }
-
-    // Helper function for populating tree with all FarmItems in root
-    private static void populateTree(TreeItem troot, FarmItem iroot) {
-        TreeItem<FarmItem> addedItem = new TreeItem<>(iroot);
-        troot.getChildren().add(addedItem);
-        if (iroot.isContainer) {
-            for (Object e : iroot.getContainedItems()) {
-                populateTree(addedItem, (FarmItem) e);
-            }
-        }
-    }
-
-    private void expandTreeView(TreeItem<FarmItem> item) {
-        if (item != null && !item.isLeaf()) {
-            item.setExpanded(true);
-            for (TreeItem<FarmItem> child : item.getChildren()) {
-                expandTreeView(child);
-            }
-        }
-    }
-
-    private void testAdds() {
-        FarmItem farm = new FarmItem(true, 50, 50, 250, 100, 50, "Farm", itemsRoot);
-        farm.setParent(itemsRoot);
-        itemsRoot.addChildItem(farm);
-        FarmItem cow = new FarmItem(false, 90, 20, 40, 20, 50, "Cow", farm);
-        farm.addChildItem(cow);
-        cow.setParent(farm);
-        FarmItem silo = new FarmItem(true, 400, 150, 90, 300, 50, "Silo", itemsRoot);
-        itemsRoot.addChildItem(silo);
-        silo.setParent(itemsRoot);
-    }
-
-    private void updateConfigPanel() {
-        configPane.setHgap(10);
-        configPane.setVgap(10);
-
-        // Labels for each field
-        Label nameLabel = new Label("Name:");
-        Label priceLabel = new Label("Price:");
-        Label locationXLabel = new Label("Location X:");
-        Label locationYLabel = new Label("Location Y:");
-        Label widthLabel = new Label("Width:");
-        Label heightLabel = new Label("Height:");
-
-        // TextFields for input
-        nameTextField = new TextField(currentSelectedItem.getName());
-        priceTextField = new TextField(Double.toString(currentSelectedItem.price));
-        locationXTextField = new TextField(Double.toString(currentSelectedItem.getX()));
-        locationYTextField = new TextField(Double.toString(currentSelectedItem.getY()));
-        widthTextField = new TextField(Double.toString(currentSelectedItem.getWidth()));
-        heightTextField = new TextField(Double.toString(currentSelectedItem.getHeight()));
-
-        // Add labels and text fields to the GridPane
-        configPane.add(nameLabel, 0, 0);
-        configPane.add(nameTextField, 1, 0);
-        configPane.add(priceLabel, 0, 1);
-        configPane.add(priceTextField, 1, 1);
-        configPane.add(locationXLabel, 0, 2);
-        configPane.add(locationXTextField, 1, 2);
-        configPane.add(locationYLabel, 0, 3);
-        configPane.add(locationYTextField, 1, 3);
-        configPane.add(widthLabel, 0, 4);
-        configPane.add(widthTextField, 1, 4);
-        configPane.add(heightLabel, 0, 5);
-        configPane.add(heightTextField, 1, 5);
-        configPane.add(addAsChildCheckBox, 0, 6);
-        configPane.add(isContainer, 1, 6);
-        configPane.add(saveConfigBtn, 0, 7);
-        configPane.add(deleteConfigBtn, 1, 7);
-        configPane.add(visitSelectedWithDroneBtn, 0, 8);
-    }
-
     // Getters and Setters
     public static DashboardSingleton getInstance() {
         return instance;
     }
 
-    public void setCurrentSelectedItem(FarmItem item) {
-        instance.currentSelectedItem = item;
-    }
-
-    public FarmItem getCurrentSelectedItem() {
-        return instance.currentSelectedItem;
+    public HBox getMainHBox() {
+        return this.mainHBox;
     }
 }
